8233727f748694b8024eecf2fbdaafe0225c31b7
Yashhh03 <yashsurya8888@gmail.com>
2024-08-06 21:58:03 +0530

    working with webhooks

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs";
 const Page = () => {
   return (
     <div className="h-[calc(100vh-96px)] flex items-center justify-center">
-      <SignIn />;
+      <SignIn />
     </div>
   );
 };
--- src/components/AddPost.tsx
+++ src/components/AddPost.tsx
@@ -1,6 +1,10 @@
+import prisma from "@/lib/client";
+import { auth } from "@clerk/nextjs/server";
 import Image from "next/image";
 
 const AddPost = () => {
+  const { userId } = auth();
+
   return (
     <div className="p-4 bg-white shadow-md rounded-lg flex gap-4 justify-between text-sm">
       {/* Avatar */}
@@ -14,10 +18,11 @@ const AddPost = () => {
       {/* Post */}
       <div className="flex-1">
         {/* Text Input */}
-        <div className="flex gap-4">
+        <form action="" className="flex gap-4">
           <textarea
             placeholder="Post Description"
             className="flex-1 bg-slate-100 rounded-lg p-2"
+            name="desc"
           ></textarea>
           <Image
             src="/emoji.png"
@@ -26,7 +31,8 @@ const AddPost = () => {
             height={20}
             className="w-5 h-5 cursor-pointer self-end"
           />
-        </div>
+          <button>Send</button>
+        </form>
         {/* Post Options */}
         <div className="flex items-center gap-4 mt-4 text-gray-400 flex-wrap">
           <div className="flex items-center gap-2 cursor-pointer">
